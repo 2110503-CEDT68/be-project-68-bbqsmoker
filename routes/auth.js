5fc08992f3fc2e0adaeda1964ff3712b0a90997f
faiwanttorest <ysb.project01@gmail.com>
2026-03-03 17:40:31 +0700

    Fixed-Log-Out-Error

--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,5 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect,getMe);
 router.get('/logout', logout);
+router.post('/logout', protect, logout);
 module.exports = router;
